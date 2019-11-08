507fb0b8c5b9b59271d59991f4a1e1dec3fd4611
Oksana <kenaoxy@gmail.com>
2019-11-08 16:25:49 +0200

    Studying day 4

--- Lesson1_hometask/script.js
+++ Lesson1_hometask/script.js
@@ -17,8 +17,59 @@ let appData = {
     optionalExpenses: {},
     income: [],
     timeData: date,
-    savings: true
-}
+    savings: true,
+    chooseExpences: function () {
+        for (let i = 0; i < 2; i++) {
+            let a = prompt("Enter required expences this months"),
+                b = +prompt("What summ of money is needed?");
+            if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null
+                && a != "" && b != "" && a.length < 50) {
+                console.log("Data is correct");
+                appData.expenses[a] = b;
+            } else {
+                i--;
+            }
+        }
+    },
+    detectDayBudget: function () {
+        appData.moneyPerDay = (appData.budget / 30).toFixed(2);
+        alert("Money per day:" + appData.moneyPerDay);
+    },
+    detectLevel: function () {
+        if (appData.moneyPerDay < 100) {
+            console.log("Minimum level of income");
+        } else if (appData.moneyPerDay >= 100 && appData.moneyPerDay < 1000) {
+            console.log("Middle level of income");
+        } else if (appData.moneyPerDay >= 1000) {
+            console.log("High level of income");
+        } else console.log("Some error occured");
+    },
+    checkSavings: function () {
+        if (appData.savings == true) {
+            let save = +prompt("What summ of savings do you have?"),
+                percent = +prompt("What percent?");
+            appData.monthIncome = save / 100 / 12 * percent;
+            alert("Your monthly income is: " + appData.monthIncome);
+        }
+    },
+    chooseOptExpenses: function () {
+        for (let i = 0; i < 3; i++) {
+            let a = +prompt("What summ of money is needed for optional expences?");
+            if (typeof (a) != null && a != "") {
+                console.log("done");
+                appData.optionalExpenses[i] = a;
+            }
+        }
+        console.log(appData.optionalExpenses)
+    },
+    chooseIncome: function () {
+        let items = prompt("What does give you additional income? (use ',' )", "");
+        appData.income = items.split(', ');
+        appData.income.push(prompt("Something else?"));
+        appData.income.sort();
+    }
+};
+
 
 /*a1 = prompt("Enter required expences this months");
 a11 = prompt("What summ of money is needed?");
@@ -28,62 +79,17 @@ a21 = prompt("What summ of money is needed?");
 appData.expenses.a1 = a11;
 appData.expenses.a2 = a21;*/
 
-function chooseExpences(){
-    for (let i = 0; i < 2; i++){
-        let a = prompt("Enter required expences this months"),
-            b = +prompt("What summ of money is needed?");
-        if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null
-            && a != "" && b != "" && a.length < 50) {
-            console.log ("done");
-            appData.expenses[a] = b;
-        }
-}
-
 /*    else {
         i--;
         continue;
     }*/
 
-}
+/*
+
 chooseExpences();
 detectDayBudget();
 detectLevel();
 chooseOptExpenses();
-
-function detectDayBudget() {
-    appData.moneyPerDay = (appData.budget / 30).toFixed(2);
-    alert("Money per day:" + appData.moneyPerDay);
-}
-
-function detectLevel() {
-    if (appData.moneyPerDay < 100){
-        console.log("Minimum level of income");
-    } else if (appData.moneyPerDay >= 100 && appData.moneyPerDay < 1000) {
-        console.log("Middle level of income");
-    } else if (appData.moneyPerDay >= 1000) {
-        console.log("High level of income");
-    } else console.log("Some error occured");
-}
-
-function chooseOptExpenses() {
-    for (let i = 0; i < 3; i++) {
-        let a = +prompt("What summ of money is needed for optional expences?");
-        if (typeof (a) != null && a != "") {
-            console.log("done");
-            appData.optionalExpenses[i] = a;
-        }
-    }
-    console.log(appData.optionalExpenses)
-}
-
-function checkSavings() {
-    if (appData.savings == true){
-        let save = +prompt("What summ of savings do you have?"),
-            percent = +prompt("What percent?");
-        appData.monthIncome = save/100/12*percent;
-        alert("Your monthly income is: " + appData.monthIncome);
-    }
-}
-
 checkSavings();
+*/
 
